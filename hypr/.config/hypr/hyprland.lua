0348e080f2008c0b29f0548a0ca781c4da631ce2
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-06-12 21:22:45 +0200

    hyprland looknfeel

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -3,6 +3,7 @@ package.path = os.getenv("HOME") .. "/.config/?.lua;" .. os.getenv("PATH") .. "/
 require("hypr.helpers")
 
 require("hypr.monitors")
+require("hypr.looknfeel")
 require("hypr.autostart")
 
 require("hypr.bindings.applications")
