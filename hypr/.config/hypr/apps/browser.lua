9ddb9c962cc62dabb5ca26b755b808a971d7ded3
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-06-28 17:53:54 +0200

    added browser override hyprland

---
+++ hypr/.config/hypr/apps/browser.lua
@@ -0,0 +1,3 @@
+-- Browser tags and styling.
+a.window("([fF]irefox|zen|librewolf)", { tag = "+firefox-based-browser" })
+a.window({ tag = "firefox-based-browser" }, { tag = "-default-opacity", opacity = "1.0 0.97" })
