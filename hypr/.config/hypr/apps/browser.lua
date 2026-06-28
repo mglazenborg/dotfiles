aacc6e5cce01bc18d6682a810956e8a3fd6433e4
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-06-28 18:07:56 +0200

    browser opacity for video

--- hypr/.config/hypr/apps/browser.lua
+++ hypr/.config/hypr/apps/browser.lua
@@ -1,3 +1,6 @@
 -- Browser tags and styling.
 a.window("([fF]irefox|zen|librewolf)", { tag = "+firefox-based-browser" })
 a.window({ tag = "firefox-based-browser" }, { tag = "-default-opacity", opacity = "1.0 0.97" })
+
+-- Set opacity to 1 for video sites.
+a.window({ tag = "+firefox-based-browser", title = ".*(YouTube|Twitch).*" }, { opacity = "1.0 1.0" })
