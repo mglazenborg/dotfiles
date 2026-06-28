475747e20f99ab6ea1480a701f0ef2e038d40884
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-06-28 18:13:14 +0200

    fixing logic

--- hypr/.config/hypr/apps/browser.lua
+++ hypr/.config/hypr/apps/browser.lua
@@ -3,4 +3,7 @@ a.window("([fF]irefox|zen|librewolf)", { tag = "+firefox-based-browser" })
 a.window({ tag = "firefox-based-browser" }, { tag = "-default-opacity", opacity = "1.0 0.97" })
 
 -- Set opacity to 1 for video sites.
-a.window({ tag = "firefox-based-browser", title = ".*(YouTube|Twitch).*" }, { opacity = "1.0 1.0" })
+a.window(
+	{ tag = "firefox-based-browser", title = ".*(YouTube|Twitch).*" },
+	{ tag = "-default-opacity", opacity = "1.0 1.0" }
+)
