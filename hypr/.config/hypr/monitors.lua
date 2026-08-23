7bc4e1114141600275442ee3776455d3e40f54e6
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-08-23 11:52:18 +0200

    more changes

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -6,3 +6,11 @@ local monitor_scale = "auto"
 
 hl.env("GDK_SCALE", tostring(gdk_scale))
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = monitor_scale })
+
+for i = 1, 10 do
+  hl.workspace_rule({
+    workspace = tostring(i),
+    monitor   = i <= 5 and "DP-2" or "DP-1",
+    default   = i == 1 or i == 6,
+  })
+end
